0e76bbe6d83142b5667a5257185f98ee3b529971
root <root@ip-10-0-50-19.ap-southeast-1.compute.internal>
2019-09-10 15:50:08 +0900

    完成？

--- 20190910.sh
+++ 20190910.sh
@@ -2,10 +2,10 @@
 
 BK_DATE=`date +'%Y%m%d'`
 BK_NAME=/root/git/menta
-DELETE_DATE=`/bin/date -d '8 day ago' +%Y%m%d`
+DL_DATE=`/bin/date -d '8 day ago' +%Y%m%d`
 
 mysqldump --single-transaction -u root -pAokipiza0027# menta > $BK_NAME/menta_$BK_DATE.dump > /dev/null 2>&1
 
 aws s3 cp $BK_NAME/menta_$BK_DATE.dump s3://menta-backup/
 
-aws s3 rm s3://menta-backup/menta_$DELETE_DATE.dump
+aws s3 rm s3://menta-backup/menta_$DL_DATE.dump
